05357c750361df5aa29d21f6776290e794a620d3
alumno <alumno@eim-alu-83022.lab.unavarra.es>
2024-04-16 11:23:04 +0200

    Updated

---
+++ src/main/java/com/mycompany/guerra/Guerra.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guerra;
+
+/**
+ *
+ * @author alumno
+ */
+public class Guerra {
+    
+    public void repartir(JuegoCartas juego, int jugadores) {
+        
+    }
+
+    public static void main(String[] args) {
+        
+        JuegoCartas juego = new JuegoCartas();
+        
+        
+        
+        
+    }
+}
